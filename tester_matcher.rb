163eaa0b773fac142374a6016cb41b66649ec211
Linus Falck-Ytter <lifayt@gmail.com>
2016-07-28 16:32:37 -0400

    prepping for rails

--- tester_matcher.rb
+++ tester_matcher.rb
@@ -7,7 +7,7 @@ class TesterMatcher
 		@pool = TestingPool.new
 	end
 
-	# Takes country shortcuts and an array of device Id's
+	# Takes country shortcuts and an array of device Id's. Passing an array allows single or multiple selection.
 	def match(countries = [], devices = [])
 		testers_list = testers.find_all {|tester| countries.include? (tester.country) and tester.devices.any? {|device| devices.include? (device)}}
 		testers_list.sort_by { |tester| tester.devices_bugs(tester.devices&devices)}.reverse
